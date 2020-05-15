61c98c611cb339c67bdd6ae8dfc586c23e131724
Eliseu Dantas <eliseu.dantas@fire-engine-red.com>
2020-05-15 11:07:29 -0300

    Removes unused classes

--- MaskSuppier/MaskSupplier.CrossCutting/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MaskSupplier.CrossCutting
-{
-    public class Class1
-    {
-    }
-}
--- MaskSuppier/MaskSupplier.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MaskSupplier.Domain
-{
-    public class Class1
-    {
-    }
-}
